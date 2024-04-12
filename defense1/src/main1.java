329805eb1af2b078829f6cc336129e7073e60cdd
adelwee <230883@astanait.edu.kz>
2024-04-12 09:49:41 +0500

    commit

---
+++ defense1/src/main1.java
@@ -0,0 +1,22 @@
+public static class main1 {
+    int counter;
+    String a;
+
+}
+public void getCounter() {
+    if(main1) {
+        getCounter();
+        int a= 5;
+
+
+    }
+    return counter;
+
+}
+def counter(word,char){
+
+}
+
+/*we have string,using recursive
+abraka dabra, how many times a showed  in these words
+*/
